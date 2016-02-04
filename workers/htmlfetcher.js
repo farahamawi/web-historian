551b4a9c6ec6b34b8fb961d4d9dba1b2f0992b24
pavelmp <pavelmp@gmail.com>
2016-02-04 15:55:54 -0800

    Make everything work. Still need to do cron and promises.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,13 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archive = require('../helpers/archive-helpers.js');
+var _ = require('underscore');
+
+exports.fetcher = function() {
+  //archive.createFile(archive.paths.archivedSites + '/ourfile.txt', 'Nailed it');
+  archive.readListOfUrls(function(array) {
+    archive.downloadUrls(array);
+  });
+  //setInterval(exports.fetcher,30000);
+};
+
